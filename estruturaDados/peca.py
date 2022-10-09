e75f54dbe80f70785429ff24e42f7fb446ff5cb8
Victor Hugo Duarte da Silva <victorhugod.s@hotmail.com>
2022-10-09 18:17:45 -0400

    criar x e y

--- estruturaDados/peca.py
+++ estruturaDados/peca.py
@@ -3,8 +3,10 @@ from enumPeca import tipoPeca
 
 
 class Peca:
-    def __init__(self, tipo: string):
+    def __init__(self, tipo: string,x:int,y:int):
         self.tipo = tipoPeca[tipo]
+        self.x=x
+        self.y=y
 
     def morte(self):
         del self
---
+++ library/bestMoveSoldier.py
@@ -0,0 +1,9 @@
+# Define qual o melhor movimento para soldado
+
+from estruturaDados.tabuleiro import Tabuleiro
+from estruturaDados.peca import Peca
+
+# tipos de movimentos
+
+
+def bestMoveSoldier(tabuleiro:Tabuleiro, soldado:Peca):
