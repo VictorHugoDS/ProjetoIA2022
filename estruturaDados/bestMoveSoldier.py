1beed57f56a618c3616472fc9ffd779c52b51ac5
Victor Hugo Duarte da Silva <victorhugod.s@hotmail.com>
2022-10-09 18:57:03 -0400

    Define novas estruturas

---
+++ estruturaDados/bestMoveSoldier.py
@@ -0,0 +1,19 @@
+# Define qual o melhor movimento para soldado
+
+from tabuleiro import Tabuleiro
+from peca import Peca
+
+# tipos de movimentos
+
+# Comer um mercenário -> 4
+# Permitir Rei chegar ao objetivo -> 10
+# Proteger o Rei -> 6
+# Proteger o Rei Comendo uma Peça -> 12
+# Colocar-se em perigo -> -2
+# Abrir Caminho para o Rei -> 3
+
+
+
+
+def melhorMovimentoSoldado(tabuleiro:Tabuleiro, soldado:Peca):
+    return 0
